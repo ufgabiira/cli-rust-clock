318e55c26dfffda7ffc9cd48c329176423674ea4
Gabriel Ferreira <88739299+ufgabiira@users.noreply.github.com>
2023-10-01 07:15:00 -0300

    refactor the code

--- src/main.rs
+++ src/main.rs
@@ -1,121 +1,120 @@
-use chrono::{Local, Timelike, DateTime};
 use ansi_term::Color::*;
+use chrono::{DateTime, Local, Timelike};
 use std::env;
 
-fn create_matrix(time: &String) -> [Vec<[char;3]>;3] {
-    let mut matrix: [Vec<[char;3]>; 3] = [
-        vec![],
-        vec![],
-        vec![]
-    ];
-    for i in time.chars() {
-        match i {
+fn create_matrix(time: String) -> [String; 3] {
+    let mut initial = [["#"; 8]; 3];
+
+    for (i, num) in time.chars().enumerate() {
+        match num {
             '0' => {
-                matrix[0].push(['┌', '─', '┐']);
-                matrix[1].push(['│', ' ', '│']);
-                matrix[2].push(['└', '─', '┘']);
+                initial[0][i] = "┌─┐";
+                initial[1][i] = "│ │";
+                initial[2][i] = "└─┘";
             }
             '1' => {
-                matrix[0].push([' ', '┐', ' ']);
-                matrix[1].push([' ', '│', ' ']);
-                matrix[2].push([' ', '┴', ' ']);
+                initial[0][i] = " ┐ ";
+                initial[1][i] = " │ ";
+                initial[2][i] = " ┴ ";
             }
             '2' => {
-                matrix[0].push(['╶', '─', '┐']);
-                matrix[1].push(['┌', '─', '┘']);
-                matrix[2].push(['└', '─', '╴']);
+                initial[0][i] = "╶─┐";
+                initial[1][i] = "┌─┘";
+                initial[2][i] = "└─╴";
             }
             '3' => {
-                matrix[0].push(['╶', '─', '┐']);
-                matrix[1].push(['╶', '─', '┤']);
-                matrix[2].push(['╶', '─', '┘']);
+                initial[0][i] = "╶─┐";
+                initial[1][i] = "╶─┤";
+                initial[2][i] = "╶─┘";
             }
             '4' => {
-                matrix[0].push(['╷', ' ', '╷']);
-                matrix[1].push(['└', '─', '┤']);
-                matrix[2].push([' ', ' ', '╵']);
+                initial[0][i] = "╷ ╷";
+                initial[1][i] = "└─┤";
+                initial[2][i] = "  ╵";
             }
             '5' => {
-                matrix[0].push(['┌', '─', '╴']);
-                matrix[1].push(['└', '─', '┐']);
-                matrix[2].push(['╶', '─', '┘']);
+                initial[0][i] = "┌─╴";
+                initial[1][i] = "└─┐";
+                initial[2][i] = "╶─┘";
             }
             '6' => {
-                matrix[0].push(['┌', '─', '╴']);
-                matrix[1].push(['├', '─', '┐']);
-                matrix[2].push(['└', '─', '┘']);
+                initial[0][i] = "┌─╴";
+                initial[1][i] = "├─┐";
+                initial[2][i] = "└─┘";
             }
             '7' => {
-                matrix[0].push(['╶', '─', '┐']);
-                matrix[1].push([' ', ' ', '│']);
-                matrix[2].push([' ', ' ', '╵']);
+                initial[0][i] = "╶─┐";
+                initial[1][i] = "  │";
+                initial[2][i] = "  ╵";
             }
             '8' => {
-                matrix[0].push(['┌', '─', '┐']);
-                matrix[1].push(['├', '─', '┤']);
-                matrix[2].push(['└', '─', '┘']);
+                initial[0][i] = "┌─┐";
+                initial[1][i] = "├─┤";
+                initial[2][i] = "└─┘";
             }
             '9' => {
-                matrix[0].push(['┌', '─', '┐']);
-                matrix[1].push(['└', '─', '┤']);
-                matrix[2].push([' ', ' ', '╵']);
+                initial[0][i] = "┌─┐";
+                initial[1][i] = "└─┤";
+                initial[2][i] = "  ╵";
             }
-            _ => {}
+            _ => {
+                initial[0][i] = ".";
+                initial[1][i] = ".";
+                initial[2][i] = " ";
+            }
         }
     }
-    return matrix;
+
+    let mut matrix: [String; 3] = [String::new(), String::new(), String::new()];
+
+    for i in 0..3 {
+        matrix[i] = initial[i].join("");
+    }
+
+    matrix
 }
 
-fn render(
-    matrix: &[Vec<[char;3]>;3], 
-    local: &DateTime<Local>,
-    is_twelve_fmt: bool,
-    is_pm: bool) 
-{
+fn render(matrix: [String; 3], local: DateTime<Local>, is_pm: bool, us_format: bool) {
     println!();
-    for i in 0..3 {
-        for j in 0..6 {
-            for k in 0..3{
-                print!("{}", Blue.bold().paint(matrix[i][j][k].to_string()));
-            }           
-            if [1, 3].contains(&j) {
-                if i != 2 {
-                    print!("{}", Blue.bold().paint("."));
-                } else {
-                    print!(" ");
-                }
-            }   
-        }
+
+    for (i, line) in matrix.iter().enumerate() {
+        print!("{}", Blue.bold().paint(line));
+
         if i == 0 {
-            print!("{}", Green.paint(local.format("%a %d").to_string()));
-        } else if i == 2 && is_twelve_fmt {
-            let meridiem = if is_pm { "PM" } else { "AM" };
-            print!("{}", Red.paint(meridiem));
+            println!("{}", Green.paint(local.format("%a %b").to_string()));
+        } else if i == 2 && us_format {
+            if is_pm {
+                println!("{}", Red.bold().paint("pm"));
+            } else {
+                println!("{}", Red.bold().paint("am"));
+            }
+        } else {
+            println!();
         }
-        println!();
     }
-    println!();
 }
 
 fn main() {
-    let mut is_twelve = false;   
     let local = Local::now();
-    let mut time = format!("{}", local.format("%H%M%S"));
     let (is_pm, hour) = local.hour12();
 
-    let args: Vec<String> = env::args().collect();
-    for i in args.iter() {
-        if i == "-b" {
-            is_twelve = true;
-            let mut new_hour = hour.to_string();
+    let mut time = format!("{}", local.format("%H:%M:%S"));
+    let mut us_format = false;
+
+    let args = env::args();
+
+    for arg in args {
+        if arg == "-b" {
+            us_format = true;
+
             if hour < 10 {
-                new_hour = format!("0{}", hour);
+                time = format!("0{}:{}", hour, local.format("%M:%S"));
+            } else {
+                time = format!("{}:{}", hour, local.format("%M:%S"));
             }
-             
-            time = format!("{}{}", new_hour, local.format("%M%S"));
         }
     }
 
-    let matrix = create_matrix(&time);
-    render(&matrix, &local, is_twelve, is_pm);
+    let matrix = create_matrix(time);
+    render(matrix, local, is_pm, us_format);
 }
